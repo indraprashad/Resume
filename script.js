26872047f02c4bb5e4fbf0ecd3205e441a3bf028
indra adhikari <git@github.com:indraprashad/Global-Connect.git>
2024-04-08 00:52:54 +0530

    third commit

--- script.js
+++ script.js
@@ -1,57 +1,125 @@
 /*===============Toggle navbar icon ===========*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
 
 /*===============scroll section active link===========*/
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        }
-    });
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 
-    /*===============sticky navbar===========*/
-    let header = document.querySelector('header');
-    header.classList.toggle("sticky", window.scrollY > 100);
+  /*===============sticky navbar===========*/
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    /*===============remove toggle icon and navbar when click navbar link (scroll) ===========*/
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  /*===============remove toggle icon and navbar when click navbar link (scroll) ===========*/
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
-     /*===============scroll reveal===========*/
-ScrollReveal({ 
-    //reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
+/*===============scroll reveal===========*/
+ScrollReveal({
+  //reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin:'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin:'bottom'});
-ScrollReveal().reveal('.home-content h1, .about-img', { origin:'left'});
-ScrollReveal().reveal('.home-content p, .about-content', { origin:'right'});
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 /*===============Multiple text===========*/
-const typed = new Typed('.multiple-text',{
-    strings: ['Frontend Developer', 'Backend Developer', 'Fullstack Developer'],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop: true,
-})
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Backend Developer", "Fullstack Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+/*===============Download cv===========*/
+function downloadCV() {
+  const cvUrl = "https://drive.google.com/drive/u/0/home";
+  const fileName = "Indra_Resume.pdf";
+
+  const link = document.createElement("a");
+  link.href = cvUrl;
+  link.download = fileName;
+
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+}
+
+document.getElementById("downloadCV").addEventListener("click", downloadCV);
+
+/*===============Form submit===========*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1eEX114iAUmT8Q_UrnMb9RfARToLXQOM",
+  authDomain: "indra-180f5.firebaseapp.com",
+  databaseURL: "https://indra-180f5-default-rtdb.firebaseio.com",
+  projectId: "indra-180f5",
+  storageBucket: "indra-180f5.appspot.com",
+  messagingSenderId: "272332298786",
+  appId: "1:272332298786:web:4a98524b29f58c3231487b",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var MessagesRef = firebase.database().ref("messages");
+document.getElementById("contact").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var name = getInputVal("name");
+  var email = getInputVal("email");
+  var phone = getInputVal("phone");
+  var subject = getInputVal("subject");
+  var message = getInputVal("message");
+
+  saveMessage(name, email, phone, subject, message);
+  document.getElementById("contact").reset();
+}
+
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
+
+function saveMessage(name, email, phone, subject, message) {
+  var newMessageRef = MessagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email: email,
+    phone: phone,
+    subject: subject,
+    message: message,
+  });
+}
+
